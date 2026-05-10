5b362cf41dd58d0dff3837e7de94f1b2f19d9877
Matheus de Oliveira Lima <matheusdeoliveiraes@gmail.com>
2026-05-10 13:46:07 -0300

    feat: adiciona idempotência no consumo de mensagens
    - Protege o OrderConsumer contra reentregas de mensagens (duplicidade)
    
    - Valida estado do banco de dados antes de aplicar regras de negócio

--- src/main/java/io/github/matheus_oliveira/spring_rabbitmq_order_pipeline/consumer/OrderConsumer.java
+++ src/main/java/io/github/matheus_oliveira/spring_rabbitmq_order_pipeline/consumer/OrderConsumer.java
@@ -16,6 +16,14 @@ public class OrderConsumer {
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void processOrder(Order orderMessage) throws InterruptedException {
         System.out.println("-> Mensagem recebida. Processando Pedido ID: " + orderMessage.getId());
+
+        Order order = repository.findById(orderMessage.getId()).orElseThrow();
+
+        // Checa idempotência
+        if ("PROCESSED".equals(order.getStatus())) {
+            System.out.println("-> [IDEMPOTÊNCIA] Pedido " + order.getId() + " já processado anteriormente. Ignorando mensagem duplicada.");
+            return; // Encerra o método sem lançar erro, o RabbitMQ descarta a mensagem.
+        }
         
         // Simulação de falha: Se o produto tiver "erro" no nome, lançamos uma exceção
         if (orderMessage.getProduct().toLowerCase().contains("erro")) {
@@ -24,10 +32,10 @@ public class OrderConsumer {
         }
         
         // Simula um processamento demorado
-        Thread.sleep(3000); 
+        System.out.println("-> Iniciando processamento pesado do Pedido ID: " + order.getId());
+        Thread.sleep(3000); // Simulando a lentidão da regra de negócio
 
         // Atualiza o status no banco de dados
-        Order order = repository.findById(orderMessage.getId()).orElseThrow();
         order.setStatus("PROCESSED");
         repository.save(order);
 
